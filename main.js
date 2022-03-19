dd516bc5363e461756f9d6e86698afbeb1f90e0f
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-03-19 11:16:46 -0500

    week2 NSS

---
+++ main.js
@@ -0,0 +1,12 @@
+const addition = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    const product = firstNumber * secondNumber
+    return product
+}
+
+console.log(addition(4,14))
+console.log(multiply(4,14))
